77c6917ba04e8e9c05fce77c0e7e720b00048dcd
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-07-23 07:28:59 +0800

    Update Yiyan.js

--- script/Yiyan.js
+++ script/Yiyan.js
@@ -7,8 +7,4 @@
 hostname = app.yiyan.art
 
 */
-let obj = JSON.parse($response.body);
-if (obj.user) {
-  obj.user.viptype = "4";
-}
-$done({ body: JSON.stringify(obj) });
+let obj = JSON.parse($response.body); if (obj.u) { obj.u.viptype = “4”; obj.u.device = “0”; } if (obj.product) { obj.product.permanenttitle = “终身会员”; obj.product.permanentdesc = “已解锁永久享用”; obj.product.permanentprice = “0”; obj.product.yearprice = “0”; obj.product.yeartitle = “”; obj.product.yeardesc = “”; obj.product.yearprice_origin = “0”; obj.product.permanentprice_origin = “0”; } $done({ body: JSON.stringify(obj) });
