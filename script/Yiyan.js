3a0f01e725eefb144396b0aaa97df3b924cc8dcb
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-07-23 06:35:42 +0800

    Create Yiyan.js

---
+++ script/Yiyan.js
@@ -0,0 +1,13 @@
+[rewrite_local]
+^https:\/\/app\.yiyan\.art\/yiyan\/getuserinfoandbooklist url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/Yiyan.js
+
+[mitm]
+hostname = app.yiyan.art
+
+*/
+let obj = JSON.parse($response.body);
+if (obj.user) {
+  obj.user.viptype = "3";
+  obj.user.intro = "VIP 已解锁";
+}
+$done({ body: JSON.stringify(obj) });
