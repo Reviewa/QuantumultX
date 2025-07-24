dd631adbc7ebbb2a3fd15ede48f77fd490a9ed35
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-07-25 06:01:11 +0800

    Create Foodie.js

---
+++ script/Foodie.js
@@ -0,0 +1,35 @@
+/*
+[rewrite_local]
+^https:\/\/purchase-foodie-api\.snow\.me\/v1\/purchase\/subscription\/subscriber\/status url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/Foodie.js
+
+[mitm]
+hostname = purchase-foodie-api.snow.me
+*/
+const path = "/v1/purchase/subscription/subscriber/status";
+
+if ($request.url.includes(path)) {
+    try {
+        JSON.parse($response.body);
+        const now = Date.now();
+        const expire = now + 251683320000000;
+        const body = {
+            result: {
+                products: [{
+                    managed: false,
+                    status: "ACTIVE",
+                    startDate: now,
+                    productId: "com.linecorp.Foodie.subscribe.oneyear",
+                    isTrialPeriod: true,
+                    expireDate: expire
+                }],
+                vipSegments: ["SUBSCRIPTION_FREE_ACTIVE"],
+                activated: true
+            }
+        };
+        $done({ body: JSON.stringify(body) });
+    } catch {
+        $done({});
+    }
+} else {
+    $done({});
+}
