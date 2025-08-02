68c1548db0488040fe07441e87cec29d890894a8
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-08-02 22:31:51 +0800

    更新 PureLibro.js

--- script/PureLibro.js
+++ script/PureLibro.js
@@ -7,37 +7,26 @@ hostname = api.rc-backup.com
 */
 
 let body = {
-  request_date_ms: Date.now(),
-  request_date: new Date().toISOString(),
   subscriber: {
-    non_subscriptions: {},
-    first_seen: "2025-06-18T16:30:38Z",
-    original_application_version: "184",
-    other_purchases: {},
-    management_url: null,
-    subscriptions: {
-      "vd_monthly_999": {
-        store: "app_store",
-        purchase_date: "2023-09-01T11:00:00Z",
-        ownership_type: "PURCHASED",
-        original_purchase_date: "2023-09-01T11:00:00Z",
-        expires_date: "2099-12-31T12:00:00Z"
-      }
-    },
     entitlements: {
-      "standard": {
-        store: "app_store",
-        purchase_date: "2023-09-01T11:00:00Z",
-        ownership_type: "PURCHASED",
-        original_purchase_date: "2023-09-01T11:00:00Z",
-        product_identifier: "vd_monthly_999",
-        expires_date: "2099-12-31T12:00:00Z"
+      pro: {
+        expires_date: null,
+        product_identifier: "reader.lifetime.pro",
+        purchase_date: "2024-01-01T00:00:00Z"
       }
     },
-    original_purchase_date: "2023-07-22T00:13:15Z",
-    original_app_user_id: "$RCAnonymousID:9368C18C930C4AE3AAA325933DBB9409",
-    last_seen: new Date().toISOString()
+    first_seen: "2024-01-01T00:00:00Z",
+    original_application_version: "1",
+    original_purchase_date: "2024-01-01T00:00:00Z",
+    subscriptions: {
+      "reader.lifetime.pro": {
+        purchase_date: "2024-01-01T00:00:00Z",
+        original_purchase_date: "2024-01-01T00:00:00Z",
+        ownership_type: "PURCHASED",
+        store: "app_store",
+        expires_date: null
+      }
+    }
   }
 };
-
 $done({ body: JSON.stringify(body) });
