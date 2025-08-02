0ca52e699c0818e53af82fd74aff150cfd184695
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-08-02 22:41:27 +0800

    更新 PureLibro.js

--- script/PureLibro.js
+++ script/PureLibro.js
@@ -1,32 +1,35 @@
 /*
 [rewrite_local]
-^https:\/\/api\.rc-backup\.com\/v1\/subscribers\/(RCAnonymousID%3A)?[a-zA-Z0-9]+ url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/PureLibro.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/v1\/subscribers\/(RCAnonymousID%3A)?[a-zA-Z0-9]+ url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/purelibro-pro.js
 
 [mitm]
-hostname = api.rc-backup.com
+hostname = api.revenuecat.com, api.rc-backup.com
 */
 
-let body = {
+let body = JSON.stringify({
+  request_date_ms: Date.now(),
+  request_date: new Date().toISOString(),
   subscriber: {
+    non_subscriptions: {},
+    first_seen: "2023-01-01T00:00:00Z",
+    original_application_version: "1",
     entitlements: {
       pro: {
         expires_date: null,
         product_identifier: "reader.lifetime.pro",
-        purchase_date: "2024-01-01T00:00:00Z"
+        purchase_date: "2023-01-01T00:00:00Z"
       }
     },
-    first_seen: "2024-01-01T00:00:00Z",
-    original_application_version: "1",
-    original_purchase_date: "2024-01-01T00:00:00Z",
     subscriptions: {
       "reader.lifetime.pro": {
-        purchase_date: "2024-01-01T00:00:00Z",
-        original_purchase_date: "2024-01-01T00:00:00Z",
-        ownership_type: "PURCHASED",
+        original_purchase_date: "2023-01-01T00:00:00Z",
+        purchase_date: "2023-01-01T00:00:00Z",
         store: "app_store",
+        ownership_type: "PURCHASED",
         expires_date: null
       }
     }
   }
-};
-$done({ body: JSON.stringify(body) });
+});
+
+$done({ body });
