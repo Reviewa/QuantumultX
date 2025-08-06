461e497dfd710d522816395cd30c990c15bc9329
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-08-06 10:13:57 +0800

    更新 purelibro.js

--- script/purelibro.js
+++ script/purelibro.js
@@ -5,30 +5,112 @@
 
 [mitm]
 hostname = api.rc-backup.com
-
 */
 
-let Qiq = JSON.parse($response.body);
-
-Qiq.subscriber = Qiq.subscriber || {};
-Qiq.subscriber.entitlements = {
-  pro: {
-    expires_date: "2099-12-31T23:59:59Z",
-    product_identifier: "reader.lifetime.pro",
-    purchase_date: "2023-01-01T00:00:00Z"
-  }
-};
-Qiq.subscriber.original_purchase_date = "2023-01-01T00:00:00Z";
-Qiq.subscriber.subscriptions = {
-  "reader.lifetime.pro": {
-    billing_issues_detected_at: null,
-    expires_date: "2099-12-31T23:59:59Z",
-    is_sandbox: false,
+let Qiq = {
+  request_date_ms: Date.now(),
+  request_date: new Date().toISOString(),
+  subscriber: {
+    entitlements: {
+      pro: {
+        grace_period_expires_date: null,
+        product_identifier: "reader.lifetime.pro",
+        purchase_date: "2023-01-01T00:00:00Z"
+      }
+    },
+    first_seen: "2023-01-01T00:00:00Z",
+    last_seen: new Date().toISOString(),
+    original_application_version: "1",
     original_purchase_date: "2023-01-01T00:00:00Z",
-    period_type: "lifetime",
-    purchase_date: "2023-01-01T00:00:00Z",
-    store: "app_store",
-    unsubscribe_detected_at: null
+    other_purchases: {
+      "reader.lifetime.pro": {
+        purchase_date: "2023-01-01T00:00:00Z"
+      },
+      "reader.lifetimeFamily.pro": {
+        purchase_date: "2023-01-01T00:00:00Z"
+      },
+      "com.reader.permanentlyPro": {
+        purchase_date: "2023-01-01T00:00:00Z"
+      },
+      "com.reader.autoRenewableYearly": {
+        purchase_date: "2023-01-01T00:00:00Z"
+      },
+      "com.reader.autoRenewableSeason": {
+        purchase_date: "2023-01-01T00:00:00Z"
+      },
+      "com.reader.autoRenewableMonth": {
+        purchase_date: "2023-01-01T00:00:00Z"
+      }
+    },
+    management_url: null,
+    subscriptions: {
+      "reader.lifetime.pro": {
+        billing_issues_detected_at: null,
+        expires_date: null,
+        is_sandbox: false,
+        original_purchase_date: "2023-01-01T00:00:00Z",
+        ownership_type: "PURCHASED",
+        period_type: "normal",
+        purchase_date: "2023-01-01T00:00:00Z",
+        store: "app_store",
+        unsubscribe_detected_at: null
+      },
+      "reader.lifetimeFamily.pro": {
+        billing_issues_detected_at: null,
+        expires_date: null,
+        is_sandbox: false,
+        original_purchase_date: "2023-01-01T00:00:00Z",
+        ownership_type: "PURCHASED",
+        period_type: "normal",
+        purchase_date: "2023-01-01T00:00:00Z",
+        store: "app_store",
+        unsubscribe_detected_at: null
+      },
+      "com.reader.permanentlyPro": {
+        billing_issues_detected_at: null,
+        expires_date: null,
+        is_sandbox: false,
+        original_purchase_date: "2023-01-01T00:00:00Z",
+        ownership_type: "PURCHASED",
+        period_type: "normal",
+        purchase_date: "2023-01-01T00:00:00Z",
+        store: "app_store",
+        unsubscribe_detected_at: null
+      },
+      "com.reader.autoRenewableYearly": {
+        billing_issues_detected_at: null,
+        expires_date: "2099-12-31T23:59:59Z",
+        is_sandbox: false,
+        original_purchase_date: "2023-01-01T00:00:00Z",
+        ownership_type: "PURCHASED",
+        period_type: "normal",
+        purchase_date: "2023-01-01T00:00:00Z",
+        store: "app_store",
+        unsubscribe_detected_at: null
+      },
+      "com.reader.autoRenewableSeason": {
+        billing_issues_detected_at: null,
+        expires_date: "2099-12-31T23:59:59Z",
+        is_sandbox: false,
+        original_purchase_date: "2023-01-01T00:00:00Z",
+        ownership_type: "PURCHASED",
+        period_type: "normal",
+        purchase_date: "2023-01-01T00:00:00Z",
+        store: "app_store",
+        unsubscribe_detected_at: null
+      },
+      "com.reader.autoRenewableMonth": {
+        billing_issues_detected_at: null,
+        expires_date: "2099-12-31T23:59:59Z",
+        is_sandbox: false,
+        original_purchase_date: "2023-01-01T00:00:00Z",
+        ownership_type: "PURCHASED",
+        period_type: "normal",
+        purchase_date: "2023-01-01T00:00:00Z",
+        store: "app_store",
+        unsubscribe_detected_at: null
+      }
+    }
   }
 };
 
