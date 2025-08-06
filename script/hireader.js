0f91aaa4df9acc024fc6cb3854544be4701a389d
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-08-06 17:48:06 +0800

    更新 hireader.js

--- script/hireader.js
+++ script/hireader.js
@@ -1,77 +1,62 @@
 /*
 
 [rewrite_local]
-^https:\/\/api\.rc-backup\.com\/v1\/subscribers\/.+\/offerings url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/hireader.js
-^https:\/\/api\.rc-backup\.com\/v1\/product_entitlement_mapping url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/hireader.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/hireader.js
 
 [mitm]
-hostname = api.rc-backup.com
+hostname = buy.itunes.apple.com
+
 */
 
-let Qiq = {
-  request_date_ms: Date.now(),
-  request_date: new Date().toISOString(),
-  subscriber: {
-    entitlements: {
-      pro: {
-        expires_date: "2099-09-09T09:09:09Z",
-        product_identifier: "HiReader_Annual",
-        purchase_date: "2020-09-09T09:09:09Z"
-      },
-      forever: {
-        expires_date: null,
-        product_identifier: "HiReader_Lifetime",
-        purchase_date: "2020-09-09T09:09:09Z"
+const Qiq = {
+  receipt: {
+    receipt_type: "Production",
+    bundle_id: "com.hireader.reader",
+    in_app: [
+      {
+        quantity: "1",
+        product_id: "com.reader.1year",
+        transaction_id: "2000000000000000",
+        original_transaction_id: "2000000000000000",
+        purchase_date: "2025-08-06T08:08:08Z",
+        original_purchase_date: "2025-08-06T08:08:08Z",
+        expires_date: "2099-08-06T08:08:08Z",
+        expires_date_ms: "4080000000000",
+        expires_date_pst: "2099-08-06T00:08:08Z",
+        is_in_intro_offer_period: "false",
+        is_trial_period: "false",
+        original_purchase_date_ms: "1691285288000",
+        purchase_date_ms: "1691285288000"
       }
-    },
-    first_seen: "2020-09-09T09:09:09Z",
-    original_application_version: "1",
-    original_purchase_date: "2020-09-09T09:09:09Z",
-    other_purchases: {
-      HiReader_Annual: {
-        purchase_date: "2020-09-09T09:09:09Z"
-      },
-      HiReader_Monthly: {
-        purchase_date: "2020-09-09T09:09:09Z"
-      },
-      HiReader_Lifetime: {
-        purchase_date: "2020-09-09T09:09:09Z"
-      }
-    },
-    management_url: null,
-    subscriptions: {
-      HiReader_Annual: {
-        billing_issues_detected_at: null,
-        expires_date: "2099-09-09T09:09:09Z",
-        is_sandbox: false,
-        original_purchase_date: "2020-09-09T09:09:09Z",
-        period_type: "active",
-        purchase_date: "2020-09-09T09:09:09Z",
-        store: "app_store",
-        unsubscribe_detected_at: null
-      },
-      HiReader_Monthly: {
-        billing_issues_detected_at: null,
-        expires_date: "2099-09-09T09:09:09Z",
-        is_sandbox: false,
-        original_purchase_date: "2020-09-09T09:09:09Z",
-        period_type: "active",
-        purchase_date: "2020-09-09T09:09:09Z",
-        store: "app_store",
-        unsubscribe_detected_at: null
-      },
-      HiReader_Lifetime: {
-        billing_issues_detected_at: null,
-        expires_date: null,
-        is_sandbox: false,
-        original_purchase_date: "2020-09-09T09:09:09Z",
-        period_type: "active",
-        purchase_date: "2020-09-09T09:09:09Z",
-        store: "app_store",
-        unsubscribe_detected_at: null
-      }
+    ]
+  },
+  latest_receipt_info: [
+    {
+      quantity: "1",
+      product_id: "com.reader.1year",
+      transaction_id: "2000000000000000",
+      original_transaction_id: "2000000000000000",
+      purchase_date: "2025-08-06T08:08:08Z",
+      original_purchase_date: "2025-08-06T08:08:08Z",
+      expires_date: "2099-08-06T08:08:08Z",
+      expires_date_ms: "4080000000000",
+      expires_date_pst: "2099-08-06T00:08:08Z",
+      is_in_intro_offer_period: "false",
+      is_trial_period: "false",
+      original_purchase_date_ms: "1691285288000",
+      purchase_date_ms: "1691285288000"
     }
-  }
+  ],
+  latest_receipt: "Qiq_Fake_Receipt",
+  pending_renewal_info: [
+    {
+      auto_renew_product_id: "com.reader.1year",
+      original_transaction_id: "2000000000000000",
+      product_id: "com.reader.1year",
+      auto_renew_status: "1"
+    }
+  ],
+  status: 0
 };
 
 $done({ body: JSON.stringify(Qiq) });
