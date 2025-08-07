254ab11758efc2611c01064f947dce66889df01d
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-08-07 21:04:04 +0800

    更新 Hireader.js

--- script/Hireader.js
+++ script/Hireader.js
@@ -1,41 +1,62 @@
 /*
-
 [rewrite_local]
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/Hireader.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/Hireader.js
 
 [mitm]
 hostname = buy.itunes.apple.com
-
 */
 
-let o = JSON.parse($response.body);
-let p = "com.reader.1year";
-let t = Math.floor(Date.now() / 1000);
-let e = 6707091199;
-let f = {
-    "quantity": "1",
-    "product_id": p,
-    "transaction_id": "520000000000000",
-    "original_transaction_id": "520000000000000",
-    "purchase_date_ms": `${t}000`,
-    "purchase_date": new Date(t * 1000).toUTCString(),
-    "purchase_date_pst": new Date(t * 1000).toLocaleString(),
-    "original_purchase_date": new Date(t * 1000).toUTCString(),
-    "original_purchase_date_ms": `${t}000`,
-    "original_purchase_date_pst": new Date(t * 1000).toLocaleString(),
-    "expires_date": new Date(e * 1000).toUTCString(),
-    "expires_date_ms": `${e}000`,
-    "expires_date_pst": new Date(e * 1000).toLocaleString(),
-    "web_order_line_item_id": "520000000000000",
-    "is_trial_period": "false",
-    "is_in_intro_offer_period": "false"
-};
-o.receipt.in_app = [f];
-o.latest_receipt_info = [f];
-o.pending_renewal_info = [{
-    "auto_renew_product_id": p,
-    "product_id": p,
-    "original_transaction_id": "520000000000000",
-    "auto_renew_status": "1"
-}];
-$done({ body: JSON.stringify(o) });
+let obj = {
+  status: 0,
+  environment: "Production",
+  receipt: {
+    receipt_type: "Production",
+    bundle_id: "com.reader",
+    application_version: "9999",
+    original_application_version: "1.0",
+    receipt_creation_date: "2999-09-09 09:09:09 Etc/GMT",
+    receipt_creation_date_ms: "32503626054000",
+    receipt_creation_date_pst: "2999-09-09 02:09:09 America/Los_Angeles",
+    request_date: "2999-09-09 09:09:09 Etc/GMT",
+    request_date_ms: "32503626054000",
+    request_date_pst: "2999-09-09 02:09:09 America/Los_Angeles",
+    original_purchase_date: "2023-04-10 01:33:52 Etc/GMT",
+    original_purchase_date_ms: "1681090432000",
+    original_purchase_date_pst: "2023-04-09 18:33:52 America/Los_Angeles",
+    in_app: [
+      {
+        quantity: "1",
+        product_id: "com.reader.1year",
+        transaction_id: "1000000999999999",
+        original_transaction_id: "1000000999999999",
+        purchase_date: "2999-09-09 09:09:09 Etc/GMT",
+        purchase_date_ms: "32503626054000",
+        purchase_date_pst: "2999-09-09 02:09:09 America/Los_Angeles",
+        original_purchase_date: "2999-09-09 09:09:09 Etc/GMT",
+        original_purchase_date_ms: "32503626054000",
+        original_purchase_date_pst: "2999-09-09 02:09:09 America/Los_Angeles",
+        is_trial_period: "false",
+        in_app_ownership_type: "PURCHASED"
+      }
+    ]
+  },
+  latest_receipt_info: [
+    {
+      quantity: "1",
+      product_id: "com.reader.1year",
+      transaction_id: "1000000999999999",
+      original_transaction_id: "1000000999999999",
+      purchase_date: "2999-09-09 09:09:09 Etc/GMT",
+      purchase_date_ms: "32503626054000",
+      purchase_date_pst: "2999-09-09 02:09:09 America/Los_Angeles",
+      original_purchase_date: "2999-09-09 09:09:09 Etc/GMT",
+      original_purchase_date_ms: "32503626054000",
+      original_purchase_date_pst: "2999-09-09 02:09:09 America/Los_Angeles",
+      is_trial_period: "false",
+      in_app_ownership_type: "PURCHASED"
+    }
+  ],
+  latest_receipt: "MIIFakeBase64ForHireader=="
+}
+
+$done({ body: JSON.stringify(obj) });
