8eba815686a10292f7aeb96c86ca654d3ad18794
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-08-13 13:11:34 +0800

    更新 Xiaozuokapian.js

--- script/Xiaozuokapian.js
+++ script/Xiaozuokapian.js
@@ -18,22 +18,29 @@ if (url.includes("api.rc-backup.com/v1/subscribers")) {
             "vip": {
                 "product_identifier": "card_vip",
                 "expires_date": "2099-12-31T23:59:59Z",
-                "purchase_date": "2025-08-13T05:01:00Z",
-                "is_active": true
+                "purchase_date": "2025-08-13T05:07:00Z",
+                "is_active": true,
+                "grace_period_expires_date": null,
+                "is_trial_period": false,
+                "is_in_billing_retry_period": false
             }
         };
         response.subscriber.subscriptions = {
             "card_vip": {
                 "product_identifier": "card_vip",
                 "expires_date": "2099-12-31T23:59:59Z",
-                "purchase_date": "2025-08-13T05:01:00Z",
+                "purchase_date": "2025-08-13T05:07:00Z",
                 "is_active": true,
                 "billing_issues_detected_at": null,
-                "auto_resume_date": null
+                "auto_resume_date": null,
+                "period_type": "normal",
+                "store": "app_store"
             }
         };
-        response.subscriber.last_seen = "2025-08-13T05:01:00Z";
-        response.subscriber.first_seen = "2025-08-13T05:01:00Z";
+        response.subscriber.last_seen = "2025-08-13T05:07:00Z";
+        response.subscriber.first_seen = "2025-08-13T05:07:00Z";
+        response.subscriber.original_purchase_date = "2025-08-13T05:07:00Z";
+        response.request_date = "2025-08-13T05:07:00Z";
         $done({ body: JSON.stringify(response) });
     } catch (e) {
         $done({});
