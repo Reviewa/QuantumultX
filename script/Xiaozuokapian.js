0f446198b2d5d5993e931ca6ad002b9b12aa9ef9
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-08-13 13:02:02 +0800

    更新 Xiaozuokapian.js

--- script/Xiaozuokapian.js
+++ script/Xiaozuokapian.js
@@ -18,7 +18,7 @@ if (url.includes("api.rc-backup.com/v1/subscribers")) {
             "vip": {
                 "product_identifier": "card_vip",
                 "expires_date": "2099-12-31T23:59:59Z",
-                "purchase_date": "2025-08-13T12:55:00Z",
+                "purchase_date": "2025-08-13T05:01:00Z",
                 "is_active": true
             }
         };
@@ -26,13 +26,14 @@ if (url.includes("api.rc-backup.com/v1/subscribers")) {
             "card_vip": {
                 "product_identifier": "card_vip",
                 "expires_date": "2099-12-31T23:59:59Z",
-                "purchase_date": "2025-08-13T12:55:00Z",
+                "purchase_date": "2025-08-13T05:01:00Z",
                 "is_active": true,
                 "billing_issues_detected_at": null,
                 "auto_resume_date": null
             }
         };
-        response.subscriber.last_seen = "2025-08-13T12:55:00Z";
+        response.subscriber.last_seen = "2025-08-13T05:01:00Z";
+        response.subscriber.first_seen = "2025-08-13T05:01:00Z";
         $done({ body: JSON.stringify(response) });
     } catch (e) {
         $done({});
