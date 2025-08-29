1cd4475089462357d20cbc9d10dac43d2fd2308f
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-08-29 15:27:02 +0800

    更新 Shuiyinbao.js

--- script/Shuiyinbao.js
+++ script/Shuiyinbao.js
@@ -1,5 +1,7 @@
 /*
 
+https://apps.apple.com/cn/app/%E6%B0%B4%E5%8D%B0%E5%AE%9D-%E5%85%A8%E8%83%BD%E8%A7%86%E9%A2%91%E5%9B%BE%E7%89%87%E6%B0%B4%E5%8D%B0%E7%BC%96%E8%BE%91%E5%99%A8/id1631521607
+
 [rewrite_local]
 ^https:\/\/newappapi\.fntmob\.com\/api\/v1\/qsy\/user-info url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/Shuiyinbao.js
 
