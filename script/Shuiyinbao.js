9458bd7223ed2017c4a6d52b092af5d3b92e25ff
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-08-29 15:23:19 +0800

    创建 Shuiyinbao.js

---
+++ script/Shuiyinbao.js
@@ -0,0 +1,21 @@
+/*
+
+[rewrite_local]
+^https:\/\/newappapi\.fntmob\.com\/api\/v1\/qsy\/user-info url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/Shuiyinbao.js
+
+[mitm]
+hostname = newappapi.fntmob.com
+
+*/
+
+let body = $response.body;
+let obj = JSON.parse(body);
+
+if (obj && obj.data) {
+  obj.data.is_vip = 1;
+  obj.data.is_buy = 1;
+  obj.data.grade = 1;
+  obj.data.level_expire = 4102415999;
+}
+
+$done({ body: JSON.stringify(obj) });
