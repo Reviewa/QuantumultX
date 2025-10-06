2dc75d14fc3a211ed1a2c0f97943a02769c63be4
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-10-06 13:36:35 +0800

    更新 IScreen.js

--- script/IScreen.js
+++ script/IScreen.js
@@ -1,11 +1,11 @@
 /*
 [rewrite_local]
 ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/IScreen.js
+^https:\/\/cs\.kuso\.xyz\/configs\?.* url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/IScreen.js
 
 [mitm]
-hostname = buy.itunes.apple.com
+hostname = buy.itunes.apple.com, cs.kuso.xyz
 */
-
 let obj = {
   status: 0,
   environment: "Production",
@@ -13,7 +13,7 @@ let obj = {
     receipt_type: "Production",
     app_item_id: 1534704608,
     bundle_id: "com.zerone.hidesktop",
-    application_version: "4.3.35",
+    application_version: "4.2.71",
     original_application_version: "4.0",
     receipt_creation_date: "2999-09-09 09:09:09 Etc/GMT",
     receipt_creation_date_ms: "32503626054000",
@@ -57,7 +57,31 @@ let obj = {
       in_app_ownership_type: "PURCHASED"
     }
   ],
-  latest_receipt: "MIIFakeBase64ContentForIScreenApp=="
+  latest_receipt: "MIIFakeBase64ContentForIScreenApp==="
 }
 
-$done({ body: JSON.stringify(obj) })
+$done({body: JSON.stringify(obj)})
+
+let body = $response.body
+let data = JSON.parse(body)
+
+if (data && data.data) {
+  data.data.noAds = 1
+  data.data.rewardAd = 0
+  data.data.BannerAd = 0
+  data.data.launchAd = 0
+  data.data.IsandBannerAd = 0
+  data.data.ThemeFeedAd = 0
+  data.data.FeedAd = 0
+  data.data.interstitial_ad = "0"
+  data.data.vipPopupType = 0
+  data.data.iapVipPopupEnable = 0
+  data.data.trial_duration = 315360000
+  data.data.interstitial_ad_interval = 999999
+  data.data.ShowVipClaimViewInterval = 999999
+  data.data.iapRetainPopupInterval = 999999
+  data.data.pic_lock_count = 999
+  data.data.interstitialWallpaper = 999999
+}
+
+$done({body: JSON.stringify(data)})
