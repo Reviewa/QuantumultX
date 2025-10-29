03d1a1e59584f68a66f294eaad493f0cd5e705b4
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-10-29 12:56:24 +0800

    更新 Jfxj.js

--- script/Jfxj.js
+++ script/Jfxj.js
@@ -1,5 +1,6 @@
 
 /*
+飓风相机专业版 - 解锁专业版功能
 
 [rewrite_local]
 ^https:\/\/base\.ysjf\.com\/storm_auth\/user\/self url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/main/script/Jfxj.js
@@ -10,21 +11,33 @@ hostname = base.ysjf.com
 */
 
 let body = JSON.parse($response.body);
-body.data.roles = [
-  {
-    appGroup: "buding",
-    id: "100",
-    permanent: true,
-    name: "VIP",
-    description: null,
-    expireTime: "2099-12-31 23:59:59"
+body = {
+  msg: "success",
+  data: {
+    phone: { region: "86", number: "" },
+    avatar: "",
+    phoneChangeCount: 3,
+    id: "100000",
+    nickname: "nickname",
+    roles: [
+      {
+        appGroup: "buding",
+        id: "100",
+        permanent: true,
+        name: "VIP",
+        description: null,
+        expireTime: "2099-12-31 23:59:59"
+      },
+      {
+        appGroup: "buding_scan",
+        id: "101",
+        name: "SVIP",
+        description: null,
+        expireTime: "2099-12-31 23:59:59"
+      }
+    ],
+    identifications: []
   },
-  {
-    appGroup: "buding_scan",
-    id: "101",
-    name: "SVIP",
-    description: null,
-    expireTime: "2099-12-31 23:59:59"
-  }
-];
+  code: 200
+};
 $done({ body: JSON.stringify(body) });
