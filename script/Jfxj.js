f2bb5e18458a821b4acea42d83a948ef7de1d821
Reviewa <141471346+Reviewa@users.noreply.github.com>
2025-10-29 12:37:11 +0800

    更新 Jfxj.js

--- script/Jfxj.js
+++ script/Jfxj.js
@@ -1,3 +1,4 @@
+
 /*
 
 [rewrite_local]
@@ -9,33 +10,21 @@ hostname = base.ysjf.com
 */
 
 let body = JSON.parse($response.body);
-body = {
-  msg: "success",
-  data: {
-    phone: { region: "86", number: "" },
-    avatar: "",
-    phoneChangeCount: 3,
-    id: "100000",
-    nickname: "nickname",
-    roles: [
-      {
-        appGroup: "buding",
-        id: "100",
-        permanent: true,
-        name: "VIP",
-        description: null,
-        expireTime: "2099-12-31 23:59:59"
-      },
-      {
-        appGroup: "buding_scan",
-        id: "101",
-        name: "SVIP",
-        description: null,
-        expireTime: "2099-12-31 23:59:59"
-      }
-    ],
-    identifications: []
+body.data.roles = [
+  {
+    appGroup: "buding",
+    id: "100",
+    permanent: true,
+    name: "VIP",
+    description: null,
+    expireTime: "2099-12-31 23:59:59"
   },
-  code: 200
-};
+  {
+    appGroup: "buding_scan",
+    id: "101",
+    name: "SVIP",
+    description: null,
+    expireTime: "2099-12-31 23:59:59"
+  }
+];
 $done({ body: JSON.stringify(body) });
