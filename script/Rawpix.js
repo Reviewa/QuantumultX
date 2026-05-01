ffbea8bcfee1ae446c9979d8f27b83c851e0cda4
Reviewa <141471346+Reviewa@users.noreply.github.com>
2026-05-02 02:33:22 +0800

    创建 Rawpix.js

---
+++ script/Rawpix.js
@@ -0,0 +1,15 @@
+/*
+^https?:\/\/api\.rawpixlive\.com\/api\/sign\/profile url script-response-body https://raw.githubusercontent.com/Reviewa/QuantumultX/script/main/RawPix.js
+[mitm]
+hostname = api.rawpixlive.com
+*/
+var Qiq = JSON.parse($response.body);
+
+Qiq.data.isCreator = 1;
+Qiq.data.is_vip = true;
+Qiq.data.expireTime = 4092599349;
+Qiq.data.level = 6;
+Qiq.data.level_name = "年卡会员";
+Qiq.data.can_trail = false;
+
+$done({ body: JSON.stringify(Qiq) });
